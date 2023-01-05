30b0f9b840ea4f6d3d6124adecf5b342fd7ac42d
shivtara <122012204+Shivtara@users.noreply.github.com>
2023-01-05 18:54:30 +0530

    Update streamlit.app.py
    updated  unicode

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 st.title ('My MOM\s new healthy Dinner')
-st.header('U+1F469','Hot dog Breakfast Menu')
+st.header("\U+1F469",'Hot dog Breakfast Menu')
 st.text('Curry Omega 3 & Blueberry Oatmeal')
 st.text('Sandwich Kale, Spinach & Rocket Smoothie')
 st.text('Cooking Hard-Boiled Free-Range Egg')
