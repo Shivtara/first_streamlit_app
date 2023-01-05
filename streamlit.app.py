6c55713bf91adc638ba2815ce86f0d3f13345bb0
shivtara <122012204+Shivtara@users.noreply.github.com>
2023-01-05 19:01:04 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,7 +1,8 @@
 
 import streamlit as st
 st.title ('My MOM\s new healthy Dinner')
-st.header('Breakfast Menu')
-st.text('Curry Omega 3 & Blueberry Oatmeal')
-st.text('Sandwich Kale, Spinach & Rocket Smoothie')
-st.text('Cooking Hard-Boiled Free-Range Egg')
+st.header('🥣 🥗 🐔 🥑🍞 Breakfast Menu 🥣 🥗 🐔 🥑🍞')
+st.text('🥗 Curry Omega 3 & Blueberry Oatmeal')
+st.text('🥑🍞 Sandwich Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Cooking Hard-Boiled Free-Range Egg')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
