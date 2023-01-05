7bfc6d223c71884348a217e0c444f00b532cdf5e
shivtara <122012204+Shivtara@users.noreply.github.com>
2023-01-05 18:34:21 +0530

    Update streamlit.app.py
    updated the Title name
    updated the header
    added the new line

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,3 +1,5 @@
 
-import streamlit
-streamlit.title('My First Code')
+import streamlit as st
+st.title ('Grocery Shop')
+st.header ('Welcome to our grocery')
+st.text('We sell all verities grocery with good quality and good prices')
