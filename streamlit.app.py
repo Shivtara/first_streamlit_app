6a0e1419c7f2470dacb7ef656a2495db5d234730
shivtara <122012204+Shivtara@users.noreply.github.com>
2023-01-15 21:04:13 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,5 +1,9 @@
 
 import streamlit as st
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLERROR
 st.title ("My MOM's new healthy Dinner")
 st.header('🥣 🥗 🐔 🥑🍞 Breakfast Menu 🥣 🥗 🐔 🥑🍞')
 st.text('🥗 Curry Omega 3 & Blueberry Oatmeal')
@@ -7,7 +11,7 @@ st.text('🥑🍞 Sandwich Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Cooking Hard-Boiled Free-Range Egg')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = st.multiselect("Pick Some Fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -18,7 +22,7 @@ st.dataframe(fruit_to_show)
 st.header("Fruityvice Fruit Advice!")
 fruit_choice = st.text_input('What fruit would you like information about?','apple')
 st.write('The user entered ', 'apple')
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "apple")
 #st.text(fruityvice_response.json()) # just write data to the screen
 # write your own comment -what does the next line do? 
@@ -26,7 +30,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 st.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+st.stop();
+
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("insert into fruit_load_list values ('from st')")
